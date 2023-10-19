91ad3d3352c4f3ad773f02196cbcfe1a1f878839
MaxM1211 <maximaes12@gmail.com>
2023-10-19 17:12:36 +0200

    add encoding test

---
+++ tests/encoding_test.py
@@ -0,0 +1,19 @@
+import os
+import chardet
+
+filename_eng="en/global.ini"
+filename_ger="global.ini"
+
+def getType(file):
+    bytes = min(32, os.path.getsize(file))
+    raw = open(file, 'rb').read(bytes)
+    return chardet.detect(raw)
+
+type_eng=getType(filename_eng)    
+type_ger=getType(filename_ger)    
+
+if(type_ger!=type_eng):
+    print(f"Enc. eng: {type_eng['encoding']}\n"\
+          f"Enc. ger: {type_ger['encoding']}")
+    exit(1)
+    
