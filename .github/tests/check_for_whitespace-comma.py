9af0c64a498fa9cf406232bf37ab81fffa7ee51e
Linebeck0 <76116052+Linebeck0@users.noreply.github.com>
2024-01-23 14:34:20 +0100

    rebuild

--- .github/tests/check_for_whitespace-comma.py
+++ .github/tests/check_for_whitespace-comma.py
@@ -1,29 +1,33 @@
-import re
+import sys
 
-def read_ini_file(file_path, encoding='utf-8'):
-    try:
-        with open(file_path, 'r', encoding=encoding) as file:
-            return file.readlines()
-    except FileNotFoundError:
-        print(f"Die Datei {file_path} wurde nicht gefunden.")
-        return None
-    except UnicodeDecodeError:
-        print(f"Fehler beim Dekodieren der Datei {file_path}. Überprüfen Sie die Zeichenkodierung.")
-        return None
+def check_brackets_balance(line):
+    if ' ,' in line:
+        return true
+    else:
+        return false
 
-def check_and_print_lines(file_lines):
-    if file_lines:
-        for line_number, line in enumerate(file_lines, start=1):
-            # Überprüfe, ob ein Leerzeichen vor dem Komma steht
-            if ' ,' in line:
-                print(line_number, end=', ')
+def find_bad_lines(file_content):
+    bad_lines = []
+
+    for i, line in enumerate(file_content, start=1):
+        if not check_brackets_balance(line):
+            bad_lines.append(i)
+
+    return bad_lines
 
 if __name__ == "__main__":
-    # Passe den Dateipfad und die Zeichenkodierung entsprechend an
-    ini_file_path = "live/global.ini"
-    file_encoding = "utf-8"  # Ändere dies entsprechend der tatsächlichen Zeichenkodierung
+    file_path = "live/global.ini"
 
-    lines = read_ini_file(ini_file_path, encoding=file_encoding)
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.readlines()
+        bad_lines = find_bad_lines(content)
+        if  bad_lines:
+            print("Folgende Zeilen bitte überprüfen")
+            print(bad_lines)
+            sys.exit(1)
 
-    if lines:
-        check_and_print_lines(lines)
+    except FileNotFoundError:
+        print(f"Die Datei '{file_path}' wurde nicht gefunden.")
+    except Exception as e:
+        print(f"Ein Fehler ist aufgetreten: {e}")
