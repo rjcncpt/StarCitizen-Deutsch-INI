30fa1a6b6eb42b4d426782a0e4f23fd4d53d731e
Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
2024-01-29 13:51:10 +0100

    Refactor text correction script and update .gitignore
    The script 'prepare_cig_ini.py' has been refactored to handle corrupted NBSP and text replacements more efficiently. It now also runs an executable and deletes temporary files and directories post processing. Additionally, the .gitignore file has been updated to correctly ignore the virtual environment and specific script-related files and directories.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>

--- scripts/prepare_cig_ini.py
+++ scripts/prepare_cig_ini.py
@@ -2,28 +2,8 @@
 import codecs
 import re
 import os
-
-
-def replace_bytes_in_file(input_file_path, temp_file_path):
-    with open(input_file_path, 'rb') as file:
-        content = file.read()
-
-    replacement = re.sub(b'(?<!\xc2)\xa0', b'\xc2\xa0', content)
-
-    with open(temp_file_path, 'wb') as file:
-        file.write(replacement)
-
-
-def replace_text(temp_file_path, output_file_path, replacements):
-    with codecs.open(temp_file_path, 'r', 'utf-8-sig') as infile:
-        original_text = infile.read()
-
-    for old_text, new_text in replacements.items():
-        original_text = original_text.replace(old_text, new_text)
-
-    with codecs.open(output_file_path, 'w', 'utf-8-sig') as outfile:
-        outfile.write(original_text)
-
+import subprocess
+import shutil
 
 # Define your replacements here
 replacements = {
@@ -41,7 +21,53 @@ replacements = {
     "~mission (item)": "~mission(item)"
 }
 
+exe_path = 'unp4k/unp4k.exe'  # absolut or relativ path to the unp4k.exe
+argument_data = 'C:\Program Files\Roberts Space Industries\StarCitizen\LIVE\Data.p4k'  # absolut or relativ path to the Data.p4k
+argument_ini = 'Data/Localization/english/global.ini'  # filter argument for unp4k
+data_path = 'Data'  # temporary directory that will be deleted at the end of the script
+input_file_path = data_path + '\Localization\english\global.ini'  # relative path to the extracted global.ini
+temp_file_path = 'global_temp.ini'  # temporary file that will be deleted at the end of the script
+output_file_path = 'global.ini'  # path where the fixed global.ini will be saved
 
+
+# Runs an exe file with the given arguments
+def run_exe_with_args(exe_path, args):
+    command = [exe_path] + args
+    try:
+        subprocess.run(command, check=True)
+        print(f"The executable {exe_path} has been started successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"The executable {exe_path} failed to run. Error: {str(e)}")
+    except FileNotFoundError:
+        print(f"The executable {exe_path} does not exist.")
+    except Exception as e:
+        print(f"An error occurred while running the executable {exe_path}: {str(e)}")
+
+
+# Fixes the corrupted NBSP and writes the result in a temporary file
+def replace_bytes_in_file(input_file_path, temp_file_path):
+    with open(input_file_path, 'rb') as file:
+        content = file.read()
+
+    replacement = re.sub(b'(?<!\xc2)\xa0', b'\xc2\xa0', content)
+
+    with open(temp_file_path, 'wb') as file:
+        file.write(replacement)
+
+
+# Replaces the given text parts
+def replace_text(temp_file_path, output_file_path, replacements):
+    with codecs.open(temp_file_path, 'r', 'utf-8-sig') as infile:
+        original_text = infile.read()
+
+    for old_text, new_text in replacements.items():
+        original_text = original_text.replace(old_text, new_text)
+
+    with codecs.open(output_file_path, 'w', 'utf-8-sig') as outfile:
+        outfile.write(original_text)
+
+
+# Deletes the temporary file
 def delete_file(filepath):
     try:
         os.remove(filepath)
@@ -54,10 +80,22 @@ def delete_file(filepath):
         print(f"An error occurred while deleting the file {filepath}: {str(e)}")
 
 
-input_file_path = 'global_original.ini'
-temp_file_path = 'global_temp.ini'
-output_file_path = 'global_fixed.ini'
+def delete_dir(dir_path):
+    try:
+        shutil.rmtree(dir_path)
+        print(f"The directory {dir_path} has been deleted successfully.")
+    except FileNotFoundError:
+        print(f"The directory {dir_path} does not exist.")
+    except PermissionError:
+        print(f"Permission denied for deleting the directory {dir_path}.")
+    except OSError as e:
+        print(f"Error: {e.filename} - {e.strerror}.")
+    except Exception as e:
+        print(f"An error occurred while deleting the directory {dir_path}: {str(e)}")
 
+
+run_exe_with_args(exe_path, [argument_data, argument_ini])
 replace_bytes_in_file(input_file_path, temp_file_path)
 replace_text(temp_file_path, output_file_path, replacements)
 delete_file(temp_file_path)
+delete_dir(data_path)
