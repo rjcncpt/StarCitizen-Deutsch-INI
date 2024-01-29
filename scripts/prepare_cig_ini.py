13c1a5ca35271cb3838483b9cce3af7ee1db0f8e
Lucas Greuloch <lucas.greuloch@pm.me>
2024-01-29 16:05:37 +0100

    Add script to fix text errors and update .gitignore (#445)
    * Add script to fix text errors and update .gitignore
    
    The commit involves two changes. Firstly, a script is added under the “scripts” folder to automatically correct text errors in a 'global_original.ini' file, such as misspelt and incorrect forms, and put the corrected output in 'global_fixed.ini'. Secondly, the .gitignore file is updated to include 'venv', so that the virtual environment files are ignored by Git.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
    
    * Refactor text correction script and update .gitignore
    
    The script 'prepare_cig_ini.py' has been refactored to handle corrupted NBSP and text replacements more efficiently. It now also runs an executable and deletes temporary files and directories post processing. Additionally, the .gitignore file has been updated to correctly ignore the virtual environment and specific script-related files and directories.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
    
    * Create start.bat script and update path syntax in prepare_cig_ini.py
    
    This commit introduces a new start.bat batch script designed to execute the Python script 'prepare_cig_ini.py'. Additionally, some updates have been made to the prepare_cig_ini.py script, where backslash characters in 'input_file_path' and 'argument_data' have been replaced with forward slashes to ensure compatibility across different systems.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
    
    * Add print statements to scripts/prepare_cig_ini.py for debugging
    
    This commit introduces print statements in the 'prepare_cig_ini.py' script to provide process updates during runtime. These messages indicate the success of the file reading, encoding fixing, and variables replacing processes, helping to troubleshoot and track the script operations.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
    
    * Reorder variables in scripts/prepare_cig_ini.py for cleaner code
    
    This commit rearranges the order of the variables in the 'prepare_cig_ini.py' script for better code readability and maintenance. They were moved to the top of the file for easy access and understanding, improving the overall organization of the script.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
    
    * Refactor scripts/prepare_cig_ini.py, rename functions and simplify process
    
    The 'prepare_cig_ini.py' script has been significantly refactored primarily through improved function names to better match their operations. Temporary file usage has been cut down, simplifying the file replacement process. Also, code comments were updated for enhanced clarity on script settings and internals.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
    
    * Replace print statements with logging in prepare_cig_ini.py
    
    The script 'prepare_cig_ini.py' has been updated to replace print statements with logging for better usability. This helps in better error and event handling by providing granularity in the level of logging, thus aiding in potential debugging as well. Exceptions were also updated to use simpler handling mechanisms.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
    
    * Refine comments in prepare_cig_ini.py script
    
    The comments in the 'prepare_cig_ini.py' script have been clarified and expanded for better understanding. The paths' descriptions have been updated to remove ambiguity, and the temporary directory's role is now clearly explained. These changes provide clearer information about each path's purpose and usage within the script.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
    
    ---------
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>

---
+++ scripts/prepare_cig_ini.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import codecs
+import re
+import subprocess
+import shutil
+import logging
+
+# The following paths need to fit your setup
+exe_path = 'unp4k/unp4k.exe'  # Path to the unp4k.exe
+argument_data = 'C:/Program Files/Roberts Space Industries/StarCitizen/LIVE/Data.p4k'  # Path to the Data.p4k
+
+# The following paths are internally used and do not need to be edited, but can be edited
+data_path = 'Data'  # Temporary directory used by unp4k, that will be deleted at the end of the script
+input_file_path = data_path + '/Localization/english/global.ini'  # Relative path to the extracted global.ini. Also used als filter argument for unp4k.
+output_file_path = 'global.ini'  # Path where the fixed global.ini will be saved
+
+# Define your replacements here
+replacements = {
+    "Oxygen_Screen_ ErrorButtonMessage=": "Oxygen_Screen_ErrorButtonMessage=",
+    "Tut03_Part01_Obj01b_ToStation =": "Tut03_Part01_Obj01b_ToStation=",
+    "ea_ui_matchmaking_error_CanceledByService =": "ea_ui_matchmaking_error_CanceledByService=",
+    "seachbody_obj_short_02a=": "searchbody_obj_short_02a=",
+    "shop_ui_transactionResult_04 _InvalidPlayerInventoryId=": "shop_ui_transactionResult_04_InvalidPlayerInventoryId=",
+    "shop_ui_transactionResult_05 _InventoryContainerRequestFail=": "shop_ui_transactionResult_05_InventoryContainerRequestFail=",
+    "shop_ui_transactionResult_06 _InventoryItemFail=": "shop_ui_transactionResult_06_InventoryItemFail=",
+    "~(Contractor": "~mission(Contractor",
+    "~misssion(Item)": "~mission(Item)",
+    "~mission (description)": "~mission(description)",
+    "~mission (title)": "~mission(title)",
+    "~mission (item)": "~mission(item)"
+}
+
+
+# Runs an exe file with the given arguments
+def run_exe(exe_path, args):
+    command = [exe_path] + args
+    try:
+        subprocess.run(command, check=True)
+        logging.info("The executable %s has been started successfully.", exe_path)
+    except subprocess.CalledProcessError as e:
+        logging.error("The executable %s failed to run. Error: %s ", exe_path, str(e))
+    except FileNotFoundError:
+        logging.error("The executable %s does not exist.", exe_path)
+    except Exception as e:
+        logging.error("An error occurred while running the executable %s: %s", exe_path, str(e))
+
+
+# Fixes the corrupted NBSP and writes the result in a temporary file
+def fix_ini(input_file_path, output_file_path):
+    try:
+        with open(input_file_path, 'rb') as file:
+            content = file.read()
+
+        replacement = re.sub(b'(?<!\xc2)\xa0', b'\xc2\xa0', content)
+        original_text = codecs.decode(replacement, 'UTF-8-SIG')
+        for old_text, new_text in replacements.items():
+            original_text = original_text.replace(old_text, new_text)
+
+        with codecs.open(output_file_path, 'w', 'UTF-8-SIG') as outfile:
+            outfile.write(original_text)
+
+        logging.info("Successfully fixed variables and wrote file as UTF-8-BOM to %s", output_file_path)
+
+    except Exception as e:
+        logging.error("An error occurred while fixing the INI file: %s", str(e))
+
+
+def delete_dir(dir_path):
+    try:
+        shutil.rmtree(dir_path)
+        logging.info("The directory %s has been deleted successfully.", dir_path)
+    except FileNotFoundError:
+        logging.error("The directory %s does not exist.", dir_path)
+    except PermissionError:
+        logging.error("Permission denied for deleting the directory %s.", dir_path)
+    except OSError as e:
+        logging.error("Error: %s - %s", e.filename, e.strerror)
+    except Exception as e:
+        logging.error("An error occurred while deleting the directory %s: %s", dir_path, str(e))
+
+
+run_exe(exe_path, [argument_data, input_file_path])
+fix_ini(input_file_path, output_file_path)
+delete_dir(data_path)
