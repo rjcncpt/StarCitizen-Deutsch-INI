3078b17cba665ba38914b0724f1fc08974f0ca6e
Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
2024-01-31 10:29:08 +0100

    Refine bracket test exit status handling
    The code enhances the bracket testing scheme by now returning an exit status, which facilitates for other scripts to act suitably based on the outcomes. The exit status is set to 0 for success (no errors) and 1 when errors such as improper bracket closure are detected.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>

--- .github/tests/bracket_test.py
+++ .github/tests/bracket_test.py
@@ -7,7 +7,9 @@ def check_brackets(filename) -> int:
         for line_number, line in enumerate(file, start=1):
             # Checks if all opened brackets are closed in the same line
             if re.search(r'\([^\)]*$', line):
-                if line_number != 44749 or line_number != 46617:
+                if line_number != 44749 and line_number != 46617:
+                    result = 0
+                else:
                     print(f"Line {line_number}: Open bracket is not closed in the same line.")
                     result = 1
     return result
