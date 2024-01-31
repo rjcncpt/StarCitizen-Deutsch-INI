4ad95f4db38c8b745558206d741c16492fdfa587
Lucas Greuloch (greluc) <lucas.greuloch@pm.me>
2024-01-31 10:26:24 +0100

    Update bracket test to return an exit status
    The bracket testing script has been updated to return an exit status indicating whether any improperly closed brackets were detected. This change allows scripts utilizing this function to react appropriately based on the results. Exit code 0 represents no errors, while 1 signals errors were found.
    
    Signed-off-by: Lucas Greuloch (greluc) <lucas.greuloch@pm.me>

--- .github/tests/bracket_test.py
+++ .github/tests/bracket_test.py
@@ -1,19 +1,26 @@
 import re
 
 
-def check_brackets(filename):
+def check_brackets(filename) -> int:
+    result = 0
     with open(filename, 'r') as file:
         for line_number, line in enumerate(file, start=1):
             # Checks if all opened brackets are closed in the same line
             if re.search(r'\([^\)]*$', line):
                 if line_number != 44749 or line_number != 46617:
                     print(f"Line {line_number}: Open bracket is not closed in the same line.")
+                    result = 1
+    return result
 
 
 if __name__ == "__main__":
     deu_live_file = "live/global.ini"
     deu_ptu_file = "ptu/global.ini"
+    exit_code = 0
     print("Checking {}...".format(deu_live_file))
-    check_brackets(deu_live_file)
+    if check_brackets(deu_live_file) != 0:
+        exit_code = 1
     print("Checking {}...".format(deu_ptu_file))
-    check_brackets(deu_ptu_file)
+    if check_brackets(deu_ptu_file) != 0:
+        exit_code = 1
+    exit(exit_code)
