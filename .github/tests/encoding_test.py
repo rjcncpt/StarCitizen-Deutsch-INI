d3c939afd5ba7da776f5cf9daa789af54a764373
MaxM1211 <maximaes12@gmail.com>
2024-04-11 21:35:57 +0200

    Remove encoding check of ptu

--- .github/tests/encoding_test.py
+++ .github/tests/encoding_test.py
@@ -27,8 +27,8 @@ file_deu_ptu = "ptu/global.ini"
 
 type_eng_live = get_type(file_eng_live)
 type_deu_live = get_type(file_deu_live)
-type_eng_ptu = get_type(file_eng_ptu)
-type_deu_ptu = get_type(file_deu_ptu)
+# type_eng_ptu = get_type(file_eng_ptu)
+# type_deu_ptu = get_type(file_deu_ptu)
 
 exit_code = 0
 
@@ -40,12 +40,12 @@ if type_eng_live["encoding"] != type_deu_live["encoding"]:
 else:
     print("The encoding between the LIVE files matches.")
 
-if type_eng_ptu["encoding"] != type_deu_ptu["encoding"]:
-    print("The encoding between the PTU INI files differs!")
-    print(f"Encoding ENG: {type_eng_ptu['encoding']}")
-    print(f"Encoding DEU: {type_deu_ptu['encoding']}")
-    exit_code = 1
-else:
-    print("The encoding between the PTU files matches.")
+# if type_eng_ptu["encoding"] != type_deu_ptu["encoding"]:
+#     print("The encoding between the PTU INI files differs!")
+#     print(f"Encoding ENG: {type_eng_ptu['encoding']}")
+#     print(f"Encoding DEU: {type_deu_ptu['encoding']}")
+#     exit_code = 1
+# else:
+#     print("The encoding between the PTU files matches.")
 
 exit(exit_code)
