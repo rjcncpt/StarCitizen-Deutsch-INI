1de24a84569bc7d54733475353ccfa6c125ccbca
MaxM1211 <61012098+MaxM1211@users.noreply.github.com>
2024-11-23 19:39:07 +0100

    Better test output (#607)
    Reworked Test output

--- .github/tests/key_test.py
+++ .github/tests/key_test.py
@@ -1,4 +1,20 @@
-from configparser import ConfigParser
+from configparser import ConfigParser, Error as configparserError, DuplicateOptionError
+import sys
+
+
+def parse_error(file_path: str, error: configparserError):
+    """
+    Parses an error and generates an error message. Stops the execution with error.
+
+    :param file_path: Path to a (ini) file (just for better output)
+    :param error: The error thrown by the ConfigParser
+    """
+    if type(error) == DuplicateOptionError:
+        sys.exit(
+            f"Fehler in '{file_path}': Der Key '{error.args[1]}' in Zeile {error.args[3]-1} exisitert bereits."
+        )
+    else:
+        sys.exit(f"Fehler in '{file_path}': '{error.message}'")
 
 
 def keys_in_second_ini(first_file, second_file):
@@ -8,18 +24,24 @@ def keys_in_second_ini(first_file, second_file):
     :return: True if all keys in the first ini file are present in the second ini file, False otherwise.
     """
     # Parse the two ini files
-    first_ini = ConfigParser(allow_no_value=True, delimiters=("="))
-    with open(first_file, "r", encoding="UTF-8-SIG") as file:
-        first_ini.read_string("[DEFAULT]\n" + file.read())
+    try:
+        first_ini = ConfigParser(allow_no_value=True, delimiters=("="))
+        with open(first_file, "r", encoding="UTF-8-SIG") as file:
+            first_ini.read_string("[DEFAULT]\n" + file.read())
+    except configparserError as e:
+        parse_error(first_file, e)
 
-    second_ini = ConfigParser(allow_no_value=True, delimiters=("="))
-    with open(second_file, "r", encoding="UTF-8-SIG") as file:
-        second_ini.read_string("[DEFAULT]\n" + file.read())
+    try:
+        second_ini = ConfigParser(allow_no_value=True, delimiters=("="))
+        with open(second_file, "r", encoding="UTF-8-SIG") as file:
+            second_ini.read_string("[DEFAULT]\n" + file.read())
+    except configparserError as e:
+        parse_error(second_file, e)
 
     # Check that all keys in the first ini are present in the second
     for key in first_ini.defaults():
         if key not in second_ini.defaults():
-            print(f"Key \"{key}\" missing from {second_file}.")
+            print(f"Key '{key}' missing from {second_file}.")
             return False
     return True
 
