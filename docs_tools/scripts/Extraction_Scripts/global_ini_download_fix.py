df2299abfe8ec7024fd9f05de43ce48118c061d3
rjcncpt <fwkart@gmail.com>
2025-06-26 15:55:20 +0200

    Update global_ini_download_fix.py
    - Hinzugefügt: Zusätzlich werden NNBSP ersetzt
    - Hinzugefügt: `remove_target_name` für bestimmte Zeilen
    - Hinzugefügt: `</?(EM1|EM2|EM3|EM4)([^>]*)>` werden escaped und neben der normalen extrahierten Datei als `4.2.1 PTU.9893855_escape` Datei gespeichert.
    
    * Die erzeugten ini Dateien werden dadurch stark angepasst und entsprechen nicht mehr der Originaldatei.

--- docs_tools/scripts/Extraction_Scripts/global_ini_download_fix.py
+++ docs_tools/scripts/Extraction_Scripts/global_ini_download_fix.py
@@ -3,6 +3,8 @@ import re
 import subprocess
 import shutil
 import logging
+import os
+import html
 
 print("\n")
 print("#########################################################")
@@ -10,14 +12,24 @@ print("# Extrahieren der global.ini Datei mit Umgebungsauswahl #")
 print("#########################################################")
 print("\n")
 
+# Das Script muss im unp4k Verzeichnis liegen:
+# Erforderliche Verzeichnisstruktur:
+# .
+# ├── global.ini                        # Ausgabedatei
+# └── unp4k
+#     ├── unp4k.exe                     # Erforderliche ausführbare Datei
+#     └── global_ini_download_fix.py    # Dieses Skript
+#     └── global_ini_download_fix.bat   # 
+
+
 # Die verfügbaren Umgebungen
 environments = {
     "1": "LIVE",
     "2": "PTU",
-    "3": "4.0_PREVIEW",
-    "4": "EPTU",
-    "5": "HOTFIX",
-    "6": "TECH-PREVIEW"
+    "3": "EPTU",
+    "4": "HOTFIX",
+    "5": "TECH-PREVIEW",
+    "6": "PREVIEW"
 }
 
 # Funktion zur Auswahl der Umgebung
@@ -42,13 +54,16 @@ selected_number, selected_environment = select_environment()
 # Dynamische Pfadanpassung basierend auf der Auswahl
 argument_data = f"E:/Roberts Space Industries/StarCitizen/{selected_environment}/Data.p4k"  # Pfad zur Data.p4k
 build_manifest_path = f"E:/Roberts Space Industries/StarCitizen/{selected_environment}/build_manifest.id"  # Pfad zur build_manifest.id
-output_file_path = "../global.ini"  # Pfad, unter dem die bearbeitete global.ini gespeichert wird
+output_file_path = "../global.ini"  # Pfad, unter dem die bearbeitete global.ini gespeichert wird.
 
 # Die folgenden Pfade sind intern und sollten nicht geändert werden
 data_path = "Data"  # Temporäres Verzeichnis, das von unp4k verwendet wird, wird am Ende des Skripts gelöscht
-input_file_path = data_path + "/Localization/english/global.ini"  # Relativer Pfad zur extrahierten global.ini. Wird auch als Filterargument für unp4k verwendet.
+input_file_path = data_path + "/Localization/english/global.ini"  # Relativer Pfad zur extrahierten global.ini
 
-# Ersetze Variablen hier
+# Zusätzlicher Kopierpfad der englischen Original ini für LIVE-Umgebung
+live_copy_path = r"F:\_Ablage\GitHub\StarCitizen-Deutsch-INI\.github\en\live\global.ini"
+
+# Ersetzungen
 replacements = {
     "Oxygen_Screen_ ErrorButtonMessage=": "Oxygen_Screen_ErrorButtonMessage=",
     "Tut03_Part01_Obj01b_ToStation =": "Tut03_Part01_Obj01b_ToStation=",
@@ -63,9 +78,13 @@ replacements = {
     "~mission (item)": "~mission(item)",
     "vehicl_DescMISC_Hull_B": "vehicle_DescMISC_Hull_B",
     " (Pickup1|Address)": " ~mission(Pickup1|Address)",
-    "Event_ShipTItle_TheGladius": "Event_ShipTitle_TheGladius"
+    "xs_outpost_001=Remote Outpost near ~mission(NearbyLocation)": "xs_outpost_001=Remote Outpost ",
+    "xs_outpost_cave_001=Cave near ~mission(NearbyLocation) ": "xs_outpost_cave_001=Cave",
+    "Event_ShipTItle_TheGladius": "Event_ShipTitle_TheGladius",
+    "<EM4>mission(Destination|Address)</EM4>": "<EM4>~mission(Destination|Address)</EM4>"
 }
 
+# Liste für Präfixe, die ~mission(Location|Address) entfernen sollen
 prefixes = [
     "mtps_UGF_eliminateall_allies_desc_intro=",
     "mtps_UGF_eliminateall_allies_desc_rehire=",
@@ -86,7 +105,12 @@ prefixes = [
     "mtps_bounty_fps_desc_001=",
     "mtps_bounty_fps_desc_first_001=",
     "mtps_bounty_fps_desc_rehire_001=",
-    # Weitere Präfixe hier hinzufügen
+    "GoblinG_MicroTech_RecoverCargo_S_Desc",
+]
+
+# Liste für Präfixe, die ~mission(TargetName) entfernen sollen
+target_name_prefixes = [
+    "Vaughn_EliminateSpecific_FPS_storm_H_desc_002=",
 ]
 
 def remove_first_mission_key(lines, prefixes):
@@ -95,6 +119,16 @@ def remove_first_mission_key(lines, prefixes):
         for prefix in prefixes:
             if line.startswith(prefix):
                 line = re.sub(r'~mission\(Location\|Address\)', '', line, count=1)
+                break
+        modified_lines.append(line)
+    return modified_lines
+
+def remove_target_name(lines, target_name_prefixes):
+    modified_lines = []
+    for line in lines:
+        for prefix in target_name_prefixes:
+            if line.startswith(prefix):
+                line = re.sub(r'~mission\(TargetName\)', '', line, count=1)
                 break
         modified_lines.append(line)
     return modified_lines
@@ -108,7 +142,7 @@ def extract_build_number(manifest_path):
         change_num_match = re.search(r'"RequestedP4ChangeNum":\s*"(\d+)"', content)
 
         if branch_match and change_num_match:
-            branch = branch_match.group(1).split('-')[-1]  # Extract the version, e.g., 4.0.0
+            branch = branch_match.group(1).split('-')[-1]
             change_num = change_num_match.group(1)
             return f"{branch} {selected_environment}.{change_num}"
         else:
@@ -126,27 +160,24 @@ def move_frontend_pu_version_to_top(temp_file_path):
         frontend_line = None
         new_lines = []
 
-        # First, try to find 'Frontend_PU_Version,P='
         for index, line in enumerate(lines):
             if line.strip().startswith("Frontend_PU_Version,P="):
                 frontend_line = lines.pop(index)
                 break
 
-        # If not found, try to find 'Frontend_PU_Version='
         if not frontend_line:
             for index, line in enumerate(lines):
                 if line.strip().startswith("Frontend_PU_Version="):
                     frontend_line = lines.pop(index)
                     break
 
-        # Move the found line to the top, if found
         if frontend_line:
             new_lines.insert(0, frontend_line)
 
         with codecs.open(temp_file_path, 'w', 'utf-8-sig') as outfile:
             outfile.write("".join(new_lines + lines))
 
-        logging.info("Frontend_PU_Version“ an den Anfang in %s verschoben", temp_file_path)
+        logging.info("Frontend_PU_Version an den Anfang in %s verschoben", temp_file_path)
 
     except Exception as e:
         logging.error("Beim Verschieben von 'Frontend_PU_Version' nach oben ist ein Fehler aufgetreten: %s", str(e))
@@ -163,28 +194,44 @@ def run_exe(exe_path, args):
     except Exception as e:
         logging.error("Beim Ausführen der ausführbaren Datei %s ist ein Fehler aufgetreten: %s", exe_path, str(e))
 
+def escape_specific_html_tags(text):
+    tag_pattern = re.compile(r'</?(EM1|EM2|EM3|EM4)([^>]*)>')
+    def escape_match(match):
+        return html.escape(match.group(0))
+    return re.sub(tag_pattern, escape_match, text)
+
 def fix_ini(input_file_path, output_file_path):
     try:
         with open(input_file_path, "rb") as file:
             content = file.read()
 
         replacement = re.sub(b"(?<!\xc2)\xa0", b"\xc2\xa0", content)
+        replacement = re.sub(b"\xe2\x80\xaf", b"\xc2\xa0", replacement)
         original_text = codecs.decode(replacement, "UTF-8-SIG")
+
         for old_text, new_text in replacements.items():
             original_text = original_text.replace(old_text, new_text)
 
         lines = original_text.splitlines()
         lines = remove_first_mission_key(lines, prefixes)
+        lines = remove_target_name(lines, target_name_prefixes)
 
         with codecs.open(output_file_path, "w", "UTF-8-SIG") as outfile:
             outfile.write("\n".join(lines))
+        logging.info("Unveränderte INI-Datei gespeichert: %s", output_file_path)
 
-        logging.info("Variablen erfolgreich festgelegt und Datei als UTF-8-BOM nach %s geschrieben", output_file_path)
+        escaped_lines = [escape_specific_html_tags(line) for line in lines]
+        escaped_path = output_file_path.replace(".ini", "_escape.ini")
+
+        with codecs.open(escaped_path, "w", "UTF-8-SIG") as escaped_file:
+            escaped_file.write("\n".join(escaped_lines))
+        logging.info("HTML-escaped INI-Datei gespeichert: %s", escaped_path)
 
         move_frontend_pu_version_to_top(output_file_path)
+        move_frontend_pu_version_to_top(escaped_path)
 
     except Exception as e:
-        logging.error("Beim Fixieren der INI-Datei ist ein Fehler aufgetreten: %s", str(e))
+        logging.error("Beim Fixieren der INI-Dateien ist ein Fehler aufgetreten: %s", str(e))
 
 def delete_dir(dir_path):
     try:
@@ -203,7 +250,10 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 run_exe(exe_path, [argument_data, input_file_path])
 build_number = extract_build_number(build_manifest_path)
-if build_number:
-    output_file_path = f"../{build_number}.ini"
-    fix_ini(input_file_path, output_file_path)
+
+# Verarbeitung der INI-Datei
+output_file_path = f"../{build_number}.ini"
+logging.info("Verarbeite INI-Datei: %s", output_file_path)
+fix_ini(input_file_path, output_file_path)
+
 delete_dir(data_path)
